ad7c49721b6735c35ba5eea14d0064e55470e944
Sunny Lou <sun24l@gmail.com>
2022-07-25 16:41:17 -0500

    bugfixes for log_parsing
    - fixed off-by-one error introduced in the attempts to clean up this code
    - added helper functions to enumerate nested folders
    - added code to compute summary statistics (count and time spent on each action) across the dataset

--- 2022_embeddings/log_parsing.py
+++ 2022_embeddings/log_parsing.py
@@ -12,21 +12,42 @@ from multiprocessing import Pool
 import datetime
 
 
+def list_folders(root_dir):
+    ''' Use if root_dir contains many folders, one for each USER_ID
+        i.e. root_dir/M####/access_log_complete.csv
+    '''
+    dir_list = [name for name in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, name))]
+    return dir_list
+
+def list_nested_paths(root_dir):
+    ''' Use if there are multiple nested folders with data
+    '''
+    outer_list = list_folders(root_dir)
+    nested_dir_list = []
+    for d in outer_list:
+        full_path = os.path.join(root_dir, d, 'provider_access_logs')
+        inner_list = list_folders(full_path) # list of M###
+        nested_dir_list += [os.path.join(d, 'provider_access_logs', f) for f in inner_list]
+    return nested_dir_list
+
 def obtain_logs(dir):
     file = os.path.join(data_dir, dir, access_file_name)
-    logs = pd.read_csv(file)
+    try:
+        logs = pd.read_csv(file)
+    except:
+        print(file + ' does not exist')
     
     # set dtype as "str" for consistency (there are some digit USER_IDs)
     logs['USER_ID'] = logs['USER_ID'].astype(str)
 
-    # identify session starts
+    # identify session ends
     logs['action'] = logs['METRIC_NAME'].map(str) + '-' + logs['REPORT_NAME'].map(str)
-    logs['interval'] = logs['ACCESS_INSTANT'].diff(periods=1).fillna(value=0)
-    chunk_starts = logs[logs['interval'] >= SESSION_BREAK * 60].index.tolist()
+    logs['interval'] = logs['ACCESS_INSTANT'].diff(periods=-1).fillna(value=0)*-1
+    chunk_ends = logs[logs['interval'] >= SESSION_BREAK * 60].index.tolist()
  
     # enumerate session IDs and label actions with their session ID 
     start = 0
-    for ind, end in enumerate(chunk_starts):
+    for ind, end in enumerate(chunk_ends):
         logs.loc[np.logical_and(logs.index >= start, logs.index < end+1), 'chunk'] = ind
         start = end + 1
     logs.loc[logs.index >= end, 'chunk'] = ind + 1
@@ -36,6 +57,8 @@ def obtain_logs(dir):
 # directories
 data_dir = '../../IGNITE/data_output_from_SSIS_2/'
 access_file_name = 'access_log_complete.csv'
+dir_list = list_folders(data_dir)
+# dir_list = list_nested_paths(data_dir)
 
 # output files
 output_dir = '../aux_data/'
@@ -50,7 +73,7 @@ SESSION_BREAK = 5  # in minutes, action gap longer than this considered as a new
 column_names = ['USER_ID', 'chunk', 'interval', 'ACCESS_TIME', 'action', 'PAT_ID']
 num_cores = multiprocessing.cpu_count()
 print('Parsing files...')
-dir_list = [name for name in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, name))]
+
 log_list = Parallel(n_jobs=num_cores)(delayed(obtain_logs)(dir) for dir in tqdm(dir_list))
 logs_all = pd.concat(log_list, axis=0)
 
@@ -60,8 +83,17 @@ action_dict = dict(zip(action_list, map(str, np.array(range(len(action_list) + 1
 inv_action_dict = {v: k for k, v in action_dict.items()}
 logs_all['action_ID'] = logs_all['action'].apply(lambda x: action_dict[x])
 
-filter_list=['USER_ID', 'chunk', 'ACCESS_TIME', 'action', 'action_ID']
-filt_logs=logs_all[filter_list]
+# computing summary statistics
+s = logs_all.groupby(by=['action_ID','action'])['interval'].agg(['sum', 'count']
+    ).sort_values('count', ascending=False).reset_index()
+s['sum'] = s['sum']/3600 # convert s to hrs
+s.rename(columns={'sum':'total_hours', 'action_ID':'token'}, inplace=True)
+# s['METRIC_NAME'] = s['action'].apply(lambda x: x.split('-')[0])
+s.to_csv('../result/action_summary.csv', index=False)
+
+# save logs (used for metric_only re-tokenization)
+# filter_list=['USER_ID', 'chunk', 'ACCESS_TIME', 'action', 'action_ID']
+# filt_logs=logs_all[filter_list]
 # filt_logs.to_csv(logs_file, index=False) # save logs for tokenize_metric_name.py
 # print('Logs saved.')
 
